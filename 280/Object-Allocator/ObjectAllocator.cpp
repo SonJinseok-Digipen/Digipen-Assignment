928214ea2b35f133026531f484a0fafc1eea1d4b
FuzzyYeti <j.son@digipen.edu>
2024-11-08 08:45:27 +0900

    .

--- 280/Object-Allocator/ObjectAllocator.cpp
+++ 280/Object-Allocator/ObjectAllocator.cpp
@@ -1,61 +1,92 @@
-#include"ObjectAllocator.h"
-#include<iostream>
-ObjectAllocator::ObjectAllocator(size_t ObjectSize,const OAConfig&config):Config_(config)
+#include "ObjectAllocator.h"
+#include <iostream>
+ObjectAllocator::ObjectAllocator(size_t ObjectSize, const OAConfig &config) : Config_(config)
 {
-  Stats_.ObjectSize_=ObjectSize;
-  Stats_.PageSize_=Calculate_Page_Size();
-  PageList_=  reinterpret_cast<GenericObject*>(new char[Stats_.PageSize_]);
-  FreeList_=  reinterpret_cast<GenericObject*>(reinterpret_cast<char*>(PageList_)+sizeof(GenericObject));
- 
-  for(size_t i=0; i<Config_.ObjectsPerPage_-1; i++)
-  {
-  GenericObject*NewFreeList_=reinterpret_cast<GenericObject*>(reinterpret_cast<char*>(FreeList_)+Stats_.ObjectSize_);
-  NewFreeList_->Next=FreeList_;
-  FreeList_=NewFreeList_;
-  }
+    Stats_.ObjectSize_ = ObjectSize;
+    Stats_.PageSize_ = Calculate_Page_Size();
+    PageList_ = reinterpret_cast<GenericObject *>(new char[Stats_.PageSize_]);
+    FreeList_ = reinterpret_cast<GenericObject *>(reinterpret_cast<char *>(PageList_) + sizeof(GenericObject));
 
+    for (size_t i = 0; i < Config_.ObjectsPerPage_ - 1; i++)
+    {
+        GenericObject *NewFreeList_ = reinterpret_cast<GenericObject *>(reinterpret_cast<char *>(FreeList_) + Stats_.ObjectSize_);
+        NewFreeList_->Next = FreeList_;
+        FreeList_ = NewFreeList_;
+    }
+    Stats_.FreeObjects_+=Config_.ObjectsPerPage_;
 }
 
 ObjectAllocator::~ObjectAllocator()
 {
-
+      while (PageList_)
+      {
+        char*temp=reinterpret_cast<char*>(PageList_);
+        PageList_=PageList_->Next;
+        delete[] temp;
+      }
 }
 
-void* ObjectAllocator::Allocate(const char*label)
+void *ObjectAllocator::Allocate(const char *label)
 {
-   return nullptr;
+    //If We have extra free space 
+    void *target = reinterpret_cast<void *>(FreeList_);
+    if (target != nullptr)
+    {
+        FreeList_ = FreeList_->Next;
+        return target;
+    }
+
+    // If we don't have free space it mean our FreeList_ point nullptr
+    else
+    {
+        GenericObject *NewPageList = reinterpret_cast<GenericObject *>(new char[Stats_.PageSize_]);
+        NewPageList->Next = PageList_;
+        PageList_ = NewPageList;
+        FreeList_ = reinterpret_cast<GenericObject *>(reinterpret_cast<char *>(PageList_) + sizeof(GenericObject));
+
+        for (size_t i = 0; i < Config_.ObjectsPerPage_ - 1; i++)
+        {
+            GenericObject *NewFreeList_ = reinterpret_cast<GenericObject *>(reinterpret_cast<char *>(FreeList_) + Stats_.ObjectSize_);
+            NewFreeList_->Next = FreeList_;
+            FreeList_ = NewFreeList_;
+        }
+         void *target = reinterpret_cast<void *>(FreeList_);
+        FreeList_ = FreeList_->Next;
+        return target;
+
+    }
 }
 
-void ObjectAllocator::Free(void*Object)
+void ObjectAllocator::Free(void *Object)
 {
+
 
 }
 
 unsigned ObjectAllocator::DumpMemoryInUse(ObjectAllocator::DUMPCALLBACK fn) const
 {
-   return 0;
+    return 0;
 }
 
 unsigned ObjectAllocator::ValidatePages(ObjectAllocator::VALIDATECALLBACK fn) const
 {
-   return 0;
+    return 0;
 }
 
 unsigned ObjectAllocator::FreeEmptyPages()
 {
-  return 0;
+    return 0;
 }
 void ObjectAllocator::SetDebugState(bool State)
 {
-
 }
 
-const void* ObjectAllocator::GetFreeList() const
+const void *ObjectAllocator::GetFreeList() const
 {
     return FreeList_;
 }
 
-const void* ObjectAllocator::GetPageList() const
+const void *ObjectAllocator::GetPageList() const
 {
     return PageList_;
 }
@@ -72,7 +103,6 @@ OAStats ObjectAllocator::GetStats() const
 
 size_t ObjectAllocator::Calculate_Page_Size()
 {
-    size_t total_size = sizeof(GenericObject*)+ (Config_.ObjectsPerPage_*Stats_.ObjectSize_);
+    size_t total_size = sizeof(GenericObject *) + (Config_.ObjectsPerPage_ * Stats_.ObjectSize_);
     return total_size;
 }
-
