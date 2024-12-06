c3b6745b6f7efe8e362d04c56a3035a1b9aeff24
FuzzyYeti <j.son@digipen.edu>
2024-12-06 18:43:30 +0900

    .

--- 280/Object-Allocator/ObjectAllocator.cpp
+++ 280/Object-Allocator/ObjectAllocator.cpp
@@ -41,6 +41,7 @@ ObjectAllocator::~ObjectAllocator()
 
 void *ObjectAllocator::Allocate([[maybe_unused]] const char *label)
 {
+
     if (FreeList_ != nullptr)
     {
         void *target = FreeList_;
@@ -94,31 +95,72 @@ void *ObjectAllocator::Allocate([[maybe_unused]] const char *label)
 
 void ObjectAllocator::Free([[maybe_unused]] void *Object)
 {
-    [[maybe_unused]] unsigned char *object = reinterpret_cast<unsigned char *>(Object);
-    if (object[0] == 0xCC)
+    unsigned char *ptr = reinterpret_cast<unsigned char *>(Object);
+    bool isAlreadyCleared = true;
+
+    // 객체의 모든 바이트가 0xCC인지 확인
+    for (size_t i = 0; i < Stats_.ObjectSize_; ++i)
     {
-        throw OAException(OAException::E_BAD_BOUNDARY, "");
+        if (ptr[i] != 0xCC)
+        {
+            isAlreadyCleared = false;
+            break;
+        }
     }
 
+    if (isAlreadyCleared)
+    {
+        // 이미 0xCC로 설정된 경우, 에러 발생
+        throw OAException(OAException::E_BAD_BOUNDARY, "Memory is already set to 0xCC");
+    }
+
+    bool onBound = false;
+    GenericObject *page = PageList_;
+
+    // 페이지 내에서 객체 주소가 올바른 경계에 있는지 확인
+    while (page != nullptr)
+    {
+        unsigned char *pageStart = reinterpret_cast<unsigned char *>(page);
+       [[maybe_unused]] unsigned char *pageEnd = pageStart + Stats_.PageSize_;
+
+        // 페이지 내에서 객체가 올바른 범위에 있는지 확인
+        if (reinterpret_cast<unsigned char *>(page) < ptr && ptr < reinterpret_cast<u_char *>(page) + Stats_.PageSize_)
+        {
+            unsigned char *blockStart = pageStart + sizeof(void *);
+                                 
+            size_t separation = Stats_.ObjectSize_;
+                                
+
+            // 객체가 블록 경계에 위치하는지 체크
+            if (0 == (ptr - blockStart) % separation)
+            {
+                onBound = true;
+                break;
+            }
+        }
+        page = page->Next;
+    }
+
+    // 만약 객체가 경계에 맞지 않으면 예외 발생
+    if (!onBound)
+    {
+        std::cout << "Error: Object not on a block boundary" << std::endl;
+        throw OAException(OAException::E_BAD_BOUNDARY, "Object is not aligned to a block boundary");
+    }
+     // FreeList_가 nullptr인 경우 새로운 프리 리스트에 객체 추가
     if (FreeList_ == nullptr)
     {
-        void *freeListStart = reinterpret_cast<unsigned char *>(PageList_) + sizeof(void *);
-        void *freeListEnd = reinterpret_cast<unsigned char *>(freeListStart) + Stats_.PageSize_;
         FreeList_ = reinterpret_cast<GenericObject *>(Object);
         FreeList_->Next = nullptr;
         Stats_.Deallocations_++;
         Stats_.ObjectsInUse_--;
         Stats_.FreeObjects_++;
-        void *target = FreeList_;
-        // 메모리가 이미 0xCC로 설정되어 있는지 검사
-
-        std::memset(target, 0xCC, Stats_.ObjectSize_);
+        std::memset(FreeList_, 0xCC, Stats_.ObjectSize_);
     }
     else
     {
         GenericObject *NewFreeList_ = reinterpret_cast<GenericObject *>(Object);
         NewFreeList_->Next = FreeList_;
-
         std::memset(NewFreeList_, 0xCC, Stats_.ObjectSize_);
         FreeList_ = NewFreeList_;
         Stats_.FreeObjects_++;
